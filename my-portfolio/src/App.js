6cd06900a53ac9f4b451e7eebe1df7ca31459ed5
omar-elhallam <omar.elhallam8@gmail.com>
2025-01-23 11:42:03 +0100

    update app.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
+import './App.css';  // Keep or modify this file for global styles
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AboutMe />
+      <Projects />
+      <Contact />
     </div>
   );
 }
