9b53ce06a344b2fc83a12d36d53283e339e50fa3
omar-elhallam <omar.elhallam8@gmail.com>
2025-02-23 16:40:14 +0000

    first commit

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';  // Import your global CSS (you can customize this file)
-import App from './App';  // Import the main App component
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />  // This is where your app's UI will be rendered
-  </React.StrictMode>,
-  document.getElementById('root')  // Attaches to the DOM element with id "root"
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
